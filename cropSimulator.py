ecbe40a365784a8f7688f51987f38c4d569843ad
Edwin31470 <31470@longroad.ac.uk>
2014-06-17 14:30:08 +0100

    task 5b and 6 done

--- cropSimulator.py
+++ cropSimulator.py
@@ -41,14 +41,73 @@ def auto_grow(crop,days):
         water = random.randint(1,10)
         crop.grow(light,water)
 
+def manual_grow(crop):
+    valid = False
+    while not valid:
+        try:
+            light = int(input("Please enter a light value (1-10): "))
+            if 1 <= light <=10:
+                valid = True
+            else:
+                print("Value entered was not valid - please enter a value between 1 and 10.")
+        except ValueError:
+             print("Value entered was not valid - please enter a value between 1 and 10.")
+    valid = False
+    while not valid:
+        try:
+            water = int(input("Please enter a water value (1-10): "))
+            if 1 <= water <=10:
+                valid = True
+            else:
+                print("Value entered was not valid - please enter a value between 1 and 10.")
+        except ValueError:
+             print("Value entered was not valid - please enter a value between 1 and 10.")
+    crop.grow(light,water)
+
+def display_menu():
+    print("1. Grow manually over 1 day")
+    print("2. Grow automatically over 10 days")
+    print("3. Report Status")
+    print("0. Exit test program")
+    print("\nPlease select an option from the menu")
+
+def get_menu_choice():
+    option_valid = False
+    while not option_valid:
+        try:
+            choice = int(input("Option Selected: "))
+            if 0 <= choice <= 4:
+                option_valid = True
+            else:
+                print("Please enter a valid option\n")
+        except ValueError:
+            print("Please enter a valid option\n")
+    return choice
+
+def manage_crop(crop):
+    print("This is the crop management program\n")
+    noexit = True
+    while noexit:
+        display_menu()
+        option = get_menu_choice()
+        print()
+        if option == 1:
+            manual_grow(crop)
+            print()
+        if option == 2:
+            auto_grow(crop,10)
+            print()
+        if option == 3:
+            print(crop.report())
+            print()
+        if option == 0:
+            noexit = False
+    print("Thank you for playing crop management!")
 
 
-    
 def main():
     new_crop = Crop(1,4,3)
-    print(new_crop.report())
-    auto_grow(new_crop,10)
-    print(new_crop.report())
+    manage_crop(new_crop)
 
 if __name__ == "__main__":
     main()
